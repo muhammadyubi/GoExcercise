8af22e06b47f51a0bd553bd27234c83f0c190ca3
muhammadyubi <114081058+muhammadyubi@users.noreply.github.com>
2022-10-27 22:36:54 +0700

    Delete Final_Project_MyGram directory

--- Final_Project_MyGram/pkg/domain/auth/handler.go
+++
@@ -1,8 +0,0 @@
-package auth
-
-import "github.com/gin-gonic/gin"
-
-type AuthHandler interface {
-	LoginUserHdl(ctx *gin.Context)
-	GetRefreshTokenHdl(ctx *gin.Context)
-}
--- Final_Project_MyGram/pkg/domain/message/error.go
+++
@@ -1,7 +0,0 @@
-package message
-
-type ErrorType string
-type ErrorMessage struct {
-	Error error     `json:"error_message,omitempty"`
-	Type  ErrorType `json:"error_type"`
-}
